34b27e898fee05a2082b89dd25c25c7a366bd5e2
ployrung <phloi.p@gmail.com>
2016-04-25 10:35:56 +0700

    Intent to SignUp finish

--- app/src/main/java/chairattanangkul/ployrung/wedplanner/MainActivity.java
+++ app/src/main/java/chairattanangkul/ployrung/wedplanner/MainActivity.java
@@ -1,7 +1,9 @@
 package chairattanangkul.ployrung.wedplanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+    }   // Main Method
+
+    public void clickSignUpMain(View view) {
+        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+
+    }   // clickSignUp ตั้ง ชื่อเอง
+
+    public void clickSignInMain(View view) {
+
+    }   // clickSignIn
+
+}   // Main Class
