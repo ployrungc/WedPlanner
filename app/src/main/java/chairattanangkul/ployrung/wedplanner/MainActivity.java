acb337e573cecf4a812fa5d237575628cb47b9b7
ployrung <phloi.p@gmail.com>
2016-04-25 14:16:30 +0700

    Delete All SQLite Finish

--- app/src/main/java/chairattanangkul/ployrung/wedplanner/MainActivity.java
+++ app/src/main/java/chairattanangkul/ployrung/wedplanner/MainActivity.java
@@ -1,6 +1,7 @@
 package chairattanangkul.ployrung.wedplanner;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,9 +21,20 @@ public class MainActivity extends AppCompatActivity {
         myManage = new MyManage(this);
 
         //Test Add Value
-        myManage.addEmail("testEmail", "testPass");
+        //myManage.addEmail("testEmail", "testPass");
+
+        //Delete All SQLite
+        deleteAllSQLite();
 
     }   // Main Method
+
+    private void deleteAllSQLite() {
+
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        sqLiteDatabase.delete(MyManage.user_table, null, null);
+
+    }   //deleteAll
 
     public void clickSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this, SignUpActivity.class));
