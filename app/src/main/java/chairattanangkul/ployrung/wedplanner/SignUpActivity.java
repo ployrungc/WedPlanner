816d92cd0058c77c9a9c165634f2dd30e103c812
ployrung <phloi.p@gmail.com>
2016-04-25 13:13:11 +0700

    Update Value to Server finish

--- app/src/main/java/chairattanangkul/ployrung/wedplanner/SignUpActivity.java
+++ app/src/main/java/chairattanangkul/ployrung/wedplanner/SignUpActivity.java
@@ -5,6 +5,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 public class SignUpActivity extends AppCompatActivity {
 
     //Explicit ประกาดตัวแปร
@@ -37,9 +47,41 @@ public class SignUpActivity extends AppCompatActivity {
 
         } else {
             //No Space
+            upLoadValueToServer();
         }
 
 
     }   //clickSignUp
+
+    private void upLoadValueToServer() {
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("Email", emailString)
+                .add("Password", passwordString)
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url("http://swiftcodingthai.com/ploy/php_add_email.php").post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+
+                try {
+                    finish();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+    }   //upload to server
 
 }   //Main Class
